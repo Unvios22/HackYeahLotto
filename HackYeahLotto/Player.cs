bd2da51165ea9900c071e448167b54685274c516
Unvios22 <unvios22@gmail.com>
2018-11-24 21:30:12 +0100

    Refactored Player class fields

--- HackYeahLotto/Player.cs
+++ HackYeahLotto/Player.cs
@@ -2,10 +2,19 @@ namespace HackYeahLotto
 {
     public class Player
     {
-        private int id;
-        private int numberofTokens;
-        private int X;
-        private int Y;
+        
+
+        private int _id;
+        private int _numberofTokens;
+        private int _positionX;
+        private int _positionY;
+
+        Player(int id, int numberOfTokens, int positionX, int positionY ) {
+            Id = id;
+            NumberofTokens = numberOfTokens;
+            PositionX = positionX;
+            PositionY = positionX;
+        }
         
         void buyTokens(int amount)
         {
@@ -16,6 +25,26 @@ namespace HackYeahLotto
         {
             //todo
             //get position i tak dalej
+        }
+        
+        public int Id {
+            get => _id;
+            set => _id = value;
+        }
+
+        public int NumberofTokens {
+            get => _numberofTokens;
+            set => _numberofTokens = value;
+        }
+
+        public int PositionX {
+            get => _positionX;
+            set => _positionX = value;
+        }
+
+        public int PositionY {
+            get => _positionY;
+            set => _positionY = value;
         }
         
     }
